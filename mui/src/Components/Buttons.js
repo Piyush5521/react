74b61cc1f1ebca3a014c81d83f083276295ca909
Piyush <piyushagarwal1408@gmail.com>
2025-02-06 14:19:47 +0530

    Add files via upload

--- mui/src/Components/Buttons.js
+++ mui/src/Components/Buttons.js
@@ -5,10 +5,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importDefault(require("react"));
 const material_1 = require("@mui/material");
+const Send_1 = __importDefault(require("@mui/icons-material/Send"));
 function Buttons() {
     return (react_1.default.createElement("div", null,
-        react_1.default.createElement(material_1.Button, { variant: "text" }, "Text"),
-        react_1.default.createElement(material_1.Button, { variant: "contained" }, "contained"),
-        react_1.default.createElement(material_1.Button, { variant: "outlined" }, "outlined")));
+        react_1.default.createElement(material_1.Stack, { spacing: 4 },
+            react_1.default.createElement(material_1.Stack, { display: 'block', spacing: 1, direction: "row" },
+                react_1.default.createElement(material_1.Button, { variant: "text" }, "Text"),
+                react_1.default.createElement(material_1.Button, { variant: "contained" }, "contained"),
+                react_1.default.createElement(material_1.Button, { variant: "outlined" }, "outlined")),
+            react_1.default.createElement(material_1.Stack, { display: 'block', spacing: 1, direction: 'row' },
+                react_1.default.createElement(material_1.Button, { variant: "contained", color: "primary" }, "Primary"),
+                react_1.default.createElement(material_1.Button, { variant: "contained", color: "secondary" }, "Secondary"),
+                react_1.default.createElement(material_1.Button, { variant: "contained", color: "error" }, "Error"),
+                react_1.default.createElement(material_1.Button, { variant: "contained", color: "warning" }, "Warning"),
+                react_1.default.createElement(material_1.Button, { variant: "contained", color: "info" }, "Info"),
+                react_1.default.createElement(material_1.Button, { variant: "contained", color: "success" }, "Success")),
+            react_1.default.createElement(material_1.Stack, { display: 'block', spacing: 2, direction: 'row' },
+                react_1.default.createElement(material_1.Button, { variant: "contained", color: "primary", size: 'small' }, "small"),
+                react_1.default.createElement(material_1.Button, { variant: "contained", color: "primary", size: 'medium' }, "medium"),
+                react_1.default.createElement(material_1.Button, { variant: "contained", color: "primary", size: 'large' }, "large")),
+            react_1.default.createElement(material_1.Stack, { display: 'block', spacing: 2, direction: 'row' },
+                react_1.default.createElement(material_1.Button, { variant: "contained", startIcon: react_1.default.createElement(Send_1.default, null), disableRipple: true, onClick: () => alert('Clicked') }, "Send"),
+                react_1.default.createElement(material_1.Button, { variant: "contained", endIcon: react_1.default.createElement(Send_1.default, null), disableElevation: true }, "Send"),
+                react_1.default.createElement(material_1.IconButton, { "aria-label": "send", color: "success", size: 'small' },
+                    react_1.default.createElement(Send_1.default, null))))));
 }
 exports.default = Buttons;
--- mui/src/Components/Buttons.tsx
+++ mui/src/Components/Buttons.tsx
@@ -1,12 +1,40 @@
 import React from "react";
-import { Button } from "@mui/material";
+import { Button, IconButton, Stack } from "@mui/material";
+import SendIcon from '@mui/icons-material/Send';
 
 function Buttons() {
   return (
     <div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">contained</Button>
-      <Button variant="outlined">outlined</Button>
+      <Stack spacing={4}>
+        <Stack display='block' spacing={1} direction="row">
+          <Button variant="text">Text</Button>
+          <Button variant="contained">contained</Button>
+          <Button variant="outlined">outlined</Button>
+        </Stack>
+
+        <Stack display='block' spacing={1} direction='row'>
+          <Button variant="contained" color="primary">Primary</Button>
+          <Button variant="contained" color="secondary">Secondary</Button>
+          <Button variant="contained" color="error">Error</Button>
+          <Button variant="contained" color="warning">Warning</Button>
+          <Button variant="contained" color="info">Info</Button>
+          <Button variant="contained" color="success">Success</Button>
+        </Stack>
+        
+        <Stack display='block' spacing={2} direction='row'>
+          <Button variant="contained" color="primary" size='small'>small</Button>
+          <Button variant="contained" color="primary" size='medium'>medium</Button>
+          <Button variant="contained" color="primary" size='large'>large</Button>
+        </Stack>
+
+        <Stack display='block' spacing={2} direction='row'>
+          <Button variant="contained" startIcon={<SendIcon />} disableRipple onClick={() => alert('Clicked')}>Send</Button>
+          <Button variant="contained" endIcon={<SendIcon />} disableElevation>Send</Button>
+          <IconButton aria-label="send" color="success" size='small'>
+            <SendIcon />
+          </IconButton>
+        </Stack>
+      </Stack>
     </div>
   );
 }
